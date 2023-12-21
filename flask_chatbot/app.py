d89e446b0820a8ba380998d7e721d9b5f740909e
Robin Faro <robinfaro@Macbook-di-Robin.local>
2023-12-21 01:08:23 +0100

    Update app.py

--- flask_chatbot/app.py
+++ flask_chatbot/app.py
@@ -4,51 +4,58 @@ import torch
 
 app = Flask(__name__)
 
+# we load the model and tokenizer, both pretrained on gpt2, putting the model on the GPU if available
 device = "cuda:1" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
 
-# Initialize tokenizer
 tokenizer = GPT2Tokenizer.from_pretrained("gpt2-large")
 tokenizer.add_special_tokens({"pad_token": "<pad>"})
 tokenizer.add_tokens(["<bot>: "])
 
-# Initialize model
 model = GPT2LMHeadModel.from_pretrained("gpt2-large")
 model.resize_token_embeddings(len(tokenizer))
 
-# Move model to the chosen device
 model = model.to(device)
 
+# we load the fine-tuned model
+model.load_state_dict(torch.load("/mnt/storage_6TB/rfaro/chatBotModels/model_state_2_large_v2.pt"))
 
-model.load_state_dict(torch.load("model_state_2_large.pt", map_location=torch.device(device)))
-
-# Function for model inference
 def infer(model, tokenizer, device, text):
+    """
+    This function performs inference using a given version of a model and its tokenzier.
+
+    Parameters:
+    - model(nn.Module): the model to use for inference
+    - tokenizer(PreTrainedTokenizer): the tokenizer to use to encode the input text and decode the output text
+    - device(str): the device to use for inference
+    - text(str): the text to use as input for the model
+    """
     model.eval()
     input = text
     input_ids = tokenizer(input, return_tensors="pt")['input_ids'].to(device)
     attention_mask = tokenizer(input, return_tensors="pt")['attention_mask'].to(device)
-    output = model.generate(input_ids=input_ids, attention_mask=attention_mask, max_length=150,
-                            num_beams=5, no_repeat_ngram_size=2, do_sample=True, 
-                            top_k=50, top_p=0.95, temperature=1)
+    output = model.generate(input_ids=input_ids, attention_mask=attention_mask, max_length=150)
         
     return tokenizer.decode(output[0], skip_special_tokens=True)
 
-# Flask routes
+
 @app.route('/')
 def index():
     return render_template('home2.html')
 
 @app.route('/infer', methods=['POST'])
 def chatbot_response():
+    """
+    This function is the endpoint for the chatbot. It receives a POST request with a JSON object containing the user input and returns a JSON object containing the chatbot response.
+    """
     if request.method == 'POST':
         user_input = request.get_json()['user_input']
         answer = infer(model, tokenizer, device, user_input)
-        answer = answer.split(user_input)[1]
+        answer = answer.split(user_input)[1] # we remove the user input from the answer since the model start from the input repeating it
         return jsonify({'bot_response': answer})
     
-# Start the Flask app
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5002, debug=True)
+
 
 
 
